b101e1fd6418869d96d2aa02a43bf7d551cb1ac2
Saqib Javed <99673719+jvedsaqib@users.noreply.github.com>
2022-10-17 19:48:16 +0530

    Update BST.java

--- BST/BST.java
+++ BST/BST.java
@@ -78,7 +78,39 @@ public class BST {
 		}
 	}
 	
+	public void postOrder(TreeNode root) {
+		if(root == null)
+			return;
+		postOrder(root.left);
+		postOrder(root.right);
+		System.out.print(root.data + " ");
+	}
 	
+	public void postOrderIT(TreeNode root) {
+		if(root == null)
+			return;
+		TreeNode current = root;
+		Stack<TreeNode> stack = new Stack<>();
+		while(current != null || !stack.isEmpty()) {
+			if(current != null) {
+				stack.push(current);
+				current = current.left;
+			} else {
+				TreeNode temp = stack.peek().right;
+				if(temp == null) {
+					temp = stack.pop();
+					System.out.print(temp.data + " ");
+					while(!stack.isEmpty() && temp == stack.peek().right) {
+						temp = stack.pop();
+						System.out.print(temp.data + " ");
+					}
+				} else {
+					current = temp;
+				}
+			}
+			
+		}
+	}
 	
 	
 
@@ -87,13 +119,23 @@ public class BST {
 		BST obj = new BST();
 		
 		obj.create();
+		System.out.println("preOrder Rec:");
 		obj.preOrder(obj.root);
 		System.out.println();
+		System.out.println("preOrder It:");
 		obj.preOrderIT(obj.root);
 		System.out.println();
+		System.out.println("inOrder It:");
 		obj.inOrder(obj.root);
 		System.out.println();
+		System.out.println("inOrder Rec:");
 		obj.inOrderIT(obj.root);
+		System.out.println();
+		System.out.println("postOrder Rec:");
+		obj.postOrder(obj.root);
+		System.out.println();
+		System.out.println("postOrder It:");
+		obj.postOrder(obj.root);
 
 	}
 
